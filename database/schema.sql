6a3cffbe6c15429ec350c70e954c3aae091b3a5b
Sean Richter <sean.richter04@outlook.com>
2019-10-27 17:53:49 -0500

    created schema and seeds.sql in database folder

---
+++ database/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE burger_db;
+USE burger_db;
+
+CREATE TABLE burger(
+	id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(50) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
