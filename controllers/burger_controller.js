0761c1f58def995b4331d6b5d31c75b7a04fee34
Sean Richter <sean.richter04@outlook.com>
2019-10-28 18:12:00 -0500

    Get Router and post router added to controller.js

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -4,12 +4,24 @@ var burger = require("../models/burger");
 //this is where we build router connections
 var router = express.Router();
 
-router.get("/", function(req, res) {
-    burger.selectAll(function(data){
+//GET Router
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
         var hdbrsObj = {
             burger: data
         };
         console.log(hdbrsObj);
-        res.render("index",hdbrsObj);
+        res.render("index", hdbrsObj);
     });
+
+    //POST Router
+    router.post("./api/burger", function (req, res) {
+        burger.insert(
+            ["burger_name", "devoured"],
+            [req.body.burger_name, req.body.devoured],
+            function (result) {
+                res.json({ id: result.insertID });
+            }
+        )
+    })
 });
