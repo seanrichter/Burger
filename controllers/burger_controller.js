0b94e8a694c8ecefe38073d3c35a846b8bf08183
Sean Richter <sean.richter04@outlook.com>
2019-10-28 18:21:21 -0500

    controller.js added

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -22,6 +22,33 @@ router.get("/", function (req, res) {
             function (result) {
                 res.json({ id: result.insertID });
             }
-        )
-    })
+        );
+    });
+    //PUT Router
+    router.put("/api/burger/:id", function (req, res) {
+        var condition = "id = " + req.params.id;
+
+        console.log("condition", condition);
+        burger.update({ devoured: req.body.devoured }, condition, function (result) {
+            if (result, changedRows === 0) {
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+        })
+    });
+    // DELETE Router
+    router.delete(condition, function (req, res) {
+        var condition = "id = " + req.params.id;
+        console.log("condition", condition);
+
+        burger.delete(condition, function (result) {
+            if (result, changedRows === 0) {
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+        });
+    });
 });
+module.exports = router;
