4670c66fa0f76f38f759ef5d19a68527ffe063a9
Sean Richter <sean.richter04@outlook.com>
2019-10-31 19:52:58 -0500

    editing connection.js to try and get application to connect to heroku

--- configuration/connection.js
+++ configuration/connection.js
@@ -9,13 +9,5 @@ connection = mysql.createConnection({
     password: "Guitarsmr1014Stella",
     database: "burger_db"
 });
-connection.connect(function(err){
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
-
-//export the module
+connection.connect();
 module.exports = connection;
