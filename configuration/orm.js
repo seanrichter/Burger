78761579a78e97ebf7ef9b2ed6a470fd7b5226ae
Sean Richter <sean.richter04@outlook.com>
2019-10-31 20:20:07 -0500

    looking at requirements, heroku still not deploying correctly

--- configuration/orm.js
+++ configuration/orm.js
@@ -1,5 +1,5 @@
 //connecting orm to connection file in configuration
-var connection = require("../configuration/connection.js");
+var connection = require("../configuration/connection");
 
 function createQmarks(num) {
     var arr = [];
