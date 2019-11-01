3f61e6329ca02755138b2ff77b9962303a031676
Sean Richter <sean.richter04@outlook.com>
2019-10-31 19:42:57 -0500

    commented out database

--- database/schema.sql
+++ database/schema.sql
@@ -1,9 +1,9 @@
-CREATE DATABASE burger_db;
-USE burger_db;
+--CREATE DATABASE burger_db;
+--USE burger_db;
 
-CREATE TABLE burger(
-    id INT NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR(50) NOT NULL,
-    devoured BOOLEAN DEFAULT false,
-    PRIMARY KEY (id)
-);
+--CREATE TABLE burger(
+ --   id INT NOT NULL AUTO_INCREMENT,
+ --   burger_name VARCHAR(50) NOT NULL,
+ --   devoured BOOLEAN DEFAULT false,
+ --   PRIMARY KEY (id)
+--);
